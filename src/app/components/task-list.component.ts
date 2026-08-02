9c216d7a75b3cd1f36fd11fd1c035116e17ce67f
Paulo-Henrique-Santana <phenrique.santana@hotmail.com>
2026-08-01 21:31:06 -0300

    fix(task-list): mover inicialização de titulo para fora do ngAfterViewInit para corrigir NG0100

--- src/app/components/task-list.component.ts
+++ src/app/components/task-list.component.ts
@@ -1,21 +1,17 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Task, TaskService } from './task.service';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html'
 })
-export class TaskListComponent implements OnInit, AfterViewInit {
+export class TaskListComponent implements OnInit {
   tasks: Task[] = [];
-  titulo: string = '';
+  titulo: string = 'Título lista';
 
   constructor(private taskService: TaskService) {}
 
   ngOnInit() {
     this.taskService.tasks$.subscribe(tasks => this.tasks = tasks);
-  }
-
-  ngAfterViewInit() {
-    this.titulo = 'Título lista';
   }
 }
